37e7f40b4fa1951d36777e0c922fe732788537f1
ahishakiyeyv <yvanahishakiye6@gmail.com>
2022-04-07 16:48:04 +0200

    details designed

---
+++ js/modal.js
@@ -0,0 +1,11 @@
+let modal=document.getElementById("modal");
+let btn=document.getElementById("details-btn");
+let closebtn=document.getElementsByClassName("close-btn")[0];
+
+btn.onclick = function(){
+    modal.classList.add("show");
+}
+
+closebtn.onclick = function(){
+    modal.classList.remove("show");
+}
