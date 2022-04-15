6ed04524e146ab279da9b06c4bb8855abdb019a7
ahishakiyeyv <yvanahishakiye6@gmail.com>
2022-04-15 15:29:46 +0200

    designing cart page

--- admin/cart.php
+++ admin/cart.php
@@ -1,5 +1,22 @@
 <?php
 include("db.php");
+if(isset($_POST["btn_update"])){
+    $update_value=$_POST["update_quantity"];
+    $update_id=$_POST["update_quantity_id"];
+    $updateQuery=$bdd->exec("UPDATE cart SET Quantite ='$update_value' WHERE id_cart ='$update_id'");
+    if($updateQuery){
+        header("location:cart.php");
+    }
+};
+if(isset($_GET['sup'])){
+    $idtodelete=$_GET['sup'];
+    $remove=$bdd->exec("DELETE FROM cart WHERE id_cart='$idtodelete'");
+    header("location:cart.php");
+};
+if(isset($_GET['delete'])){
+    $delete=$bdd->exec("DELETE FROM cart");
+    header("location:cart.php");
+}
 ?>
 
 <!DOCTYPE html>
@@ -21,9 +38,12 @@ include("db.php");
              <a href="about.php">About</a>
              <a href="login.php">My Account</a>
         </nav>
-
-        <div id="menu-btn" class="fas fa-bars"></div>
-        <a href="#" class="btn">Cart <span>0</span></a>
+        <?php
+                $select_row=$bdd->query("SELECT * FROM cart");
+                $row=$select_row->rowCount();
+            ?>
+          <div id="menu-btn" class="fas fa-bars"></div>
+          <a href="#" class="btn">Cart <span><?php echo $row;?></span></a>
    </header>
    <section class="section_1">
        <h1 class="title1">Shopping Cart</h1>
@@ -50,14 +70,14 @@ include("db.php");
                         <td><?php echo $data['nomprod']?></td>
                         <td>
                         <form method="post">
-                        <input type="hidden" name="update_quantity_id" min="1" value="<?php echo $data['id_cart']?>">
+                        <input type="hidden" name="update_quantity_id" value="<?php echo $data['id_cart']?>">
                             <input type="number" name="update_quantity" min="1" value="<?php echo $data['Quantite']?>">
                             <input type="submit" value="Update" name="btn_update">
                         </form>    
                         </td>
-                        <td><?php echo $data['prixprod']?></td>
-                        <td><?php echo $tot = number_format($data['prixprod'] * $data['Quantite']);?></td>
-                        <td><a href="#">Delete</a></td>
+                        <td><?php echo $data['prixprod']?> Fbu</td>
+                        <td><?php echo $tot = number_format($data['prixprod'] * $data['Quantite']);?>Fbu</td>
+                        <td><a href="cart.php?sup=<?php echo $data['id_cart'];?>" onclick="return confirm('Delete item from the cart')" class="remove_btn">Remove</a></td>
                     </tr>
                         <?php $grandTotal += $tot;?>
                <?php
@@ -70,7 +90,7 @@ include("db.php");
                    <td></td>
                    <td></td>
                    <td><?php echo $grandTotal?></td>
-                   <td></td>
+                   <td><a href="cart.php?delete" onclick="return confirm('Are you sure you want to delete all ?');" class="deleteAll_btn">Delete All</a></td>
                </tr>
            </tbody>
        </table>
