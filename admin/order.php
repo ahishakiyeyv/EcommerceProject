ba7ac3c93332d2ae77b9a0b4005e748663bbcc97
ahishakiyeyv <yvanahishakiye6@gmail.com>
2022-04-16 12:13:32 +0200

    order page added

--- admin/order.php
+++ admin/order.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/order.css">
-    <title>Cart | SuitSHOP</title>
+    <title>Order | SuitSHOP</title>
 </head>
 <body>
     <header class="header">
@@ -20,3 +20,43 @@
           <div id="menu-btn" class="fas fa-bars"></div>
           <a href="#" class="btn">Cart</a>
    </header>
+
+   <section class="section12">
+       <h1 class="tit1">Order Now</h1>
+       <div class="card">
+           <div class="leftside">
+                <h1>Order-Summary</h1>
+           </div>
+           <div class="rightside">
+               <h1>Checkout</h1>
+               <h2>Ordering Information</h2>
+               <form method="post">
+                   <p class="p-title">Name:</p>
+                   <input type="text" name="name" class="inpt-order">
+                   <p class="p-title">Email:</p>
+                   <input type="text" name="name" class="inpt-order">
+                   <p class="p-title">Phone:</p>
+                   <input type="number" name="name" class="inpt-order">
+                   <div class="addresse">
+                       <div class="add1">
+                       <p class="p-title">City:</p>
+                   <input type="text" name="name" class="inpt-add">
+                       </div>
+                       <div class="add1">
+                       <p class="p-title">Commune:</p>
+                   <input type="text" name="name" class="inpt-add">
+                       </div>
+                       <div class="add1">
+                       <p class="p-title">Street:</p>
+                   <input type="text" name="name" class="inpt-add">
+                       </div>
+                   </div>
+                   <input type="submit" name="submit" class="inpt-sub">
+
+               </form>
+
+           </div>
+       </div>
+   </section>
+</body>
+</html>
