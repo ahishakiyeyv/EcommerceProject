b450f2981c8e9e2ed75be212186c3f65a23a1941
ahishakiyeyv <yvanahishakiye6@gmail.com>
2022-04-25 08:46:59 +0200

    shop modified

--- admin/shop.php
+++ admin/shop.php
@@ -8,7 +8,7 @@ include("db.php");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/shops.css">
+    <link rel="stylesheet" href="css/shop.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
     <title>Shop | SuitSHOP</title>
 </head>
