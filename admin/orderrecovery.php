182aab799bdb849d44291d645ae00baeb2c60b53
ahishakiyeyv <yvanahishakiye6@gmail.com>
2022-05-01 19:47:31 +0200

    design changed

--- admin/orderrecovery.php
+++ admin/orderrecovery.php
@@ -7,23 +7,21 @@ include("db.php");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/orderrecovery.css">
-    <title>Order Recovery| FashionSHOP</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/orderrecoverys.css">
+    <title>Order Recovery | FashionSHOP</title>
 </head>
 <body>
-<header class="header">
-        <a href="#" class="logo"><i class="fas fa-splotch"></i> SuitSHOP</a>
-        <nav class="navbar">
-            <a href="dashboard.php">Dashboard</a>
-             <a href="accueil.php">Home</a>
-             <a href="shop.php">Shop</a>
-             <a href="about.php">About</a>
-             <a href="Contact.php">Contact</a>
-             <a href="login.php">My Account</a>
-        </nav>
-          <div id="menu-btn" class="fas fa-bars"></div>
-          <!-- <a href="#" class="btn">Cart</a> -->
-   </header>
+<div id="mySidenav" class="sidenav">
+	<p class="logo"><span>Fashion</span>SHOP</p>
+  <a href="dashboard.php" class="icon-a"><i class="fa fa-dashboard icons"></i> &nbsp;&nbsp;Dashboard</a>
+  <a href="customer.php"class="icon-a"><i class="fa fa-users icons"></i> &nbsp;&nbsp;Customers</a>
+  <a href="administration.php"class="icon-a"><i class="fa fa-list icons"></i> &nbsp;&nbsp;Administration</a>
+  <a href="orderrecovery.php"class="icon-a"><i class="fa fa-shopping-bag icons"></i> &nbsp;&nbsp;Orders</a>
+  <a href="#"class="icon-a"><i class="fa fa-tasks icons"></i> &nbsp;&nbsp;Cart</a>
+  <a href="admin.php"class="icon-a"><i class="fa fa-user icons"></i> &nbsp;&nbsp;Home View</a>
+  <!-- <a href="#"class="icon-a"><i class="fa fa-list-alt icons"></i> &nbsp;&nbsp;Tasks</a> -->
+</div>
    <section class="recovery">
        <h1 class="title">Order Recovery</h1>
        <div class="contain">
@@ -51,41 +49,6 @@ include("db.php");
                ?>
                </div>
    </section>
-   <footer>
-     <div class="footer-container">
-          <div class="footer-box1">
-               <h3 class="h3-ttle">SuitSHOP</h3>
-               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quos quas, dolore architecto iure illo voluptatum sequi suscipit facere quod soluta .</p>
-          </div>
-          <div class="footer-box2">
-               <h3 class="h3-ttle">Shopping</h3>
-               <ul>
-                    <li>clothing store</li>
-                    <li>Treding suit</li>
-                    <li>accessories</li>
-                    <li>Sale</li>
-               </ul>
-          </div>
-          <div class="footer-box3">
-               <h3 class="h3-ttle">Quick Links</h3>
-               <ul>
-                    <li>Contact Us</li>
-                    <li>Payment Method</li>
-                    <li>Delivery</li>
-                    <li>Return & Exchange</li>
-               </ul>
-          </div>
-          <div class="footer-box4">
-               <h3 class="h3-ttle">NewsLetter</h3>
-               <p class="p-form">Be the first to know about new arrivals, look books, sales & promos!</p>
-               <form action="" method="post">
-                    <input type="text" name="text" placeholder="Enter your email..." id="inpt-txt">
-                    <input type="submit" value="Send" name="submit" id="submit-btn">
-               </form>
-          </div>
-     </div>
-     <p class="copy">&copy 2022 All Right Reserved | </p>
-</footer>
 <?php
 include("livechat.php");
 ?>
