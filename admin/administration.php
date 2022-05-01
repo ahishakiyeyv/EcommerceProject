ddccbd83fd436b0e1e7a83521d3ad10104c5aa0b
ahishakiyeyv <yvanahishakiye6@gmail.com>
2022-05-01 19:34:54 +0200

    order recovery linked

--- admin/administration.php
+++ admin/administration.php
@@ -10,15 +10,15 @@ include("db.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/administration.css">
-    <title>Administration | SuitSHOP</title>
+    <title>Administration | FashionSHOP</title>
 </head>
 <body>
 <div id="mySidenav" class="sidenav">
-	<p class="logo"><span>E-</span>SuitSHOP</p>
+	<p class="logo"><span>Fashion</span>SHOP</p>
   <a href="dashboard.php" class="icon-a"><i class="fa fa-dashboard icons"></i> &nbsp;&nbsp;Dashboard</a>
   <a href="customer.php"class="icon-a"><i class="fa fa-users icons"></i> &nbsp;&nbsp;Customers</a>
   <a href="administration.php"class="icon-a"><i class="fa fa-list icons"></i> &nbsp;&nbsp;Administration</a>
-  <a href="#"class="icon-a"><i class="fa fa-shopping-bag icons"></i> &nbsp;&nbsp;Orders</a>
+  <a href="orderrecovery.php"class="icon-a"><i class="fa fa-shopping-bag icons"></i> &nbsp;&nbsp;Orders</a>
   <a href="#"class="icon-a"><i class="fa fa-tasks icons"></i> &nbsp;&nbsp;Cart</a>
   <a href="admin.php"class="icon-a"><i class="fa fa-user icons"></i> &nbsp;&nbsp;Home View</a>
   <!-- <a href="#"class="icon-a"><i class="fa fa-list-alt icons"></i> &nbsp;&nbsp;Tasks</a> -->
