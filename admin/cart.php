8c6dd2e523c9bacf1baa0efe858fc5d4501abebe
ahishakiyeyv <yvanahishakiye6@gmail.com>
2022-05-23 13:23:40 +0200

    cart modified

--- admin/cart.php
+++ admin/cart.php
@@ -27,7 +27,7 @@ if(isset($_GET['delete'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/cart.css">
+    <link rel="stylesheet" href="css/carts.css">
     <title>Cart | FashionSHOP</title>
 </head>
 <body>
